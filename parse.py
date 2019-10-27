68a428a8c408325161bb51f7b487e850788155da
Stepan Konstantinov <stepan@konstantinov.pl>
2019-10-27 18:05:16 +0300

    Math added

--- parse.py
+++ parse.py
@@ -17,12 +17,31 @@ class Parse:
             contestants[i]['type'] = current[-1]
         return contestants
 
+    @staticmethod
+    def mathematics(raw):
+        contestantsRaw = [line.strip().split('\t') for line in raw.strip().split('\n')]
+        contestants = [dict() for __ in range(len(contestantsRaw))]
+        for i, current in enumerate(contestantsRaw):
+            contestants[i]['place'] = int(current[0])
+            if current[1].count(' ') == 1:
+                current[1] += ' -'
+            contestants[i]['last_name'], contestants[i]['first_name'], contestants[i]['middle_name'] = current[1].split(' ')
+            contestants[i]['grade'] = int(current[2])
+            contestants[i]['school'] = current[3]
+            contestants[i]['region'] = current[4]
+            contestants[i]['score'] = int(current[-2])
+            contestants[i]['type'] = current[-1]
+        return contestants
+
 
 if __name__ == '__main__':
     file = open('contests.pickle', 'wb+')
     result = {
         'programming': {
             '20182019': Parse.programming(open('unparsed/programming20182019.txt').read())
+        },
+        'mathematics': {
+            '20182019': Parse.mathematics(open('unparsed/mathematics20182019.txt').read())
         }
     }
     pickle.dump(result, file)
