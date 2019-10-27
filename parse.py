3ab001f8b11abaebe4a5a63251153515e4b4cfff
Stepan Konstantinov <stepan@konstantinov.pl>
2019-10-27 19:16:51 +0300

    Complete admission physics

--- parse.py
+++ parse.py
@@ -33,6 +33,21 @@ class Parse:
             contestants[i]['type'] = current[-1]
         return contestants
 
+    @staticmethod
+    def physics(raw):
+        contestantsRaw = [line.strip().split('\t') for line in raw.strip().split('\n')]
+        contestants = [dict() for __ in range(len(contestantsRaw))]
+        for i, current in enumerate(contestantsRaw):
+            contestants[i]['place'] = int(current[0])
+            if current[1].count(' ') == 1:
+                current[1] += ' -'
+            contestants[i]['last_name'], contestants[i]['first_name'], contestants[i]['middle_name'] = current[1].split(' ')
+            contestants[i]['grade'] = 11
+            contestants[i]['school'] = current[2]
+            contestants[i]['type'] = current[3]
+            contestants[i]['region'] = 'Москва'
+        return contestants
+
 
 if __name__ == '__main__':
     file = open('contests.pickle', 'wb+')
@@ -42,6 +57,9 @@ if __name__ == '__main__':
         },
         'mathematics': {
             '20182019': Parse.mathematics(open('unparsed/mathematics20182019.txt').read())
+        },
+        'physics': {
+            '20182019': Parse.physics(open('unparsed/physics20182019.txt').read())
         }
     }
     pickle.dump(result, file)
--- tmp.py
+++ tmp.py
@@ -5,6 +5,6 @@ file = open('contests.pickle', 'rb')
 contests = pickle.load(file)
 file.close()
 
-for contestant in contests['mathematics']['20182019']:
+for contestant in contests['physics']['20182019']:
     if contestant['grade'] == 11:
         print(contestant['place'], contestant['last_name'], contestant['first_name'], contestant['middle_name'], contestant['region'], contestant['type'])
