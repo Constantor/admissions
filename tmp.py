3bdf803688f07d36d55edd1ef8d3fc60b980dbfe
Stepan Konstantinov <stepan@konstantinov.pl>
2019-10-27 17:22:20 +0300

    Admission base

--- tmp.py
+++ tmp.py
@@ -5,4 +5,6 @@ file = open('contests.pickle', 'rb')
 contests = pickle.load(file)
 file.close()
 
-print(contests['programming']['20182019'])
+for contestant in contests['programming']['20182019']:
+    if contestant['grade'] == 11:
+        print(contestant['place'], contestant['last_name'], contestant['first_name'], contestant['middle_name'], contestant['region'], contestant['type'])
