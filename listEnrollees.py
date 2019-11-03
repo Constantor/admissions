eecce4a8e3fc1e9b388ed3d7b50c63b18fed629a
Stepan Konstantinov <stepan@konstantinov.pl>
2019-11-03 15:37:07 +0300

    20172018 full

--- listEnrollees.py
+++ listEnrollees.py
@@ -6,9 +6,9 @@ contests = pickle.load(file)
 file.close()
 
 subject = 'programming'
-season = '20172018'
+season = '20162017'
 
 for contestant in contests[subject][season]:
-    if contestant['grade'] == 11 and 27 <= int(contestant['place']):
-        os.system('chromium https://www.google.com/search?q=' + contestant['last_name'] + '+' + contestant['first_name'] + '+' + contestant['middle_name'] + '+приказ+о+зачислении+2018')
+    if contestant['grade'] == 11:
+        os.system('chromium https://www.google.com/search?q=' + contestant['last_name'] + '+' + contestant['first_name'] + '+' + contestant['middle_name'] + '+приказ+о+зачислении+' + season[-4:])
         print(contestant['place'], contestant['last_name'], contestant['first_name'], contestant['middle_name'], contestant['region'], contestant['type'])
--- parse.py
+++ parse.py
@@ -18,7 +18,7 @@ class Parse:
 		return contestants
 
 	@staticmethod
-	def mathematics(raw):
+	def mathematics(raw, is2017=False):
 		contestantsRaw = [line.strip().split('\t') for line in raw.strip().split('\n')]
 		contestants = [dict() for __ in range(len(contestantsRaw))]
 		for i, current in enumerate(contestantsRaw):
@@ -27,8 +27,8 @@ class Parse:
 				current[1] += ' -'
 			contestants[i]['last_name'], contestants[i]['first_name'], contestants[i]['middle_name'] = current[1].split(' ')
 			contestants[i]['grade'] = int(current[2])
-			contestants[i]['school'] = current[3]
-			contestants[i]['region'] = current[4]
+			contestants[i]['school'] = current[3 if not is2017 else 4]
+			contestants[i]['region'] = current[4 if not is2017 else 4]
 			contestants[i]['score'] = int(current[-2])
 			contestants[i]['type'] = current[-1].capitalize()
 		return contestants
@@ -51,7 +51,7 @@ class Parse:
 
 if __name__ == '__main__':
 	subjects = {'programming', 'mathematics'}
-	seasons = {'20172018', '20182019'}
+	seasons = {'20162017', '20172018', '20182019'}
 
 	file = open('contests.pickle', 'wb+')
 	result = {}
@@ -61,7 +61,7 @@ if __name__ == '__main__':
 			if subject == 'programming':
 				result[subject][season] = Parse.programming(open('unparsed/programming' + season + '.txt').read())
 			elif subject == 'mathematics':
-				result[subject][season] = Parse.mathematics(open('unparsed/mathematics' + season + '.txt').read())
+				result[subject][season] = Parse.mathematics(open('unparsed/mathematics' + season + '.txt').read()) if not season == '20162017' else Parse.mathematics(open('unparsed/mathematics' + season + '.txt').read(), True)
 
 	pickle.dump(result, file)
 	file.close()
