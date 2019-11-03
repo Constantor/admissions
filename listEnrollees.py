da612db3a1358eea27c1c3819ce45122b7539ec2
Stepan Konstantinov <stepan@konstantinov.pl>
2019-11-03 13:56:17 +0300

    Admissions programming 20172018

--- listEnrollees.py
+++ listEnrollees.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 file = open('contests.pickle', 'rb')
 contests = pickle.load(file)
@@ -8,5 +9,6 @@ subject = 'programming'
 season = '20172018'
 
 for contestant in contests[subject][season]:
-    if contestant['grade'] == 11:
+    if contestant['grade'] == 11 and 27 <= int(contestant['place']):
+        os.system('chromium https://www.google.com/search?q=' + contestant['last_name'] + '+' + contestant['first_name'] + '+' + contestant['middle_name'] + '+приказ+о+зачислении+2018')
         print(contestant['place'], contestant['last_name'], contestant['first_name'], contestant['middle_name'], contestant['region'], contestant['type'])
