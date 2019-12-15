5a7587b8d9bb5df2dd374cbabaa2d032d8ec638e
Stepan Konstantinov <stepan@konstantinov.pl>
2019-12-15 17:05:17 +0300

    History admission parsed

--- listEnrollees.py
+++ listEnrollees.py
@@ -5,8 +5,8 @@ file = open('contests.pickle', 'rb')
 contests = pickle.load(file)
 file.close()
 
-subject = 'programming'
-season = '20162017'
+subject = 'history'
+season = '20182019'
 
 for contestant in contests[subject][season]:
     if contestant['grade'] == 11:
